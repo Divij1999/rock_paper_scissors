c1784ef7b84daa92b29ff4c16830b5df6aaa20c6
Divij Sumbly <divijsumbly@gmail.com>
2021-12-12 18:08:01 +0530

    Made the bare bones of the game.

--- script.js
+++ script.js
@@ -7,50 +7,62 @@ function computerPlay(){
         case 2: return "Scissor";
                 break;
     }
+    return;
 }
 
-function round(playerSelection, computerSelection){
+function playRound(e){
+        const computerSelection=computerPlay();
+        const playerSelection=this.id;
+        const result=document.querySelector(".result");
+        const round=document.querySelector(".roundcount");
+        round.innerText=`Round ${roundCount}`;
         if(playerSelection==="Rock" && computerSelection==="Scissor" || 
            playerSelection==="Paper" && computerSelection==="Rock" || 
            playerSelection==="Scissor" && computerSelection==="Paper"){
 
                 ++playerCount;
-                 return `You Win!! ${playerSelection} beats ${computerSelection}`;
+                 result.innerText=` ${playerSelection} beats ${computerSelection}
+                                   You win Round ${roundCount}`;
          }
          else if(playerSelection===computerSelection){
 
-                  return "Tie";
+                  result.innerText="Tie";
                  }
         else{ 
                 ++computerCount;
-                 return `You Lose!! ${computerSelection} beats ${playerSelection}`;
+                 result.innerText=`${computerSelection} beats ${playerSelection}
+                                    You lose Round ${roundCount}`  ;
         }
+        score();
+        return;
 }
 
-function game(){
-        for(let i=0; i!=5;++i){
-                let playerSelection=prompt("Enter rock, paper or scissor", " ").toLowerCase();
-                if(playerSelection!=="rock" && playerSelection!=="paper" && playerSelection!=="scissor"){
-                    alert("Please enter a valid value(Rock, Paper or Scissor)");
-                    --i;
-                    continue;
-                    
-                }
-                playerSelection=playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1);
-                console.log(round(playerSelection, computerPlay())); 
-                console.log(`Your Score:${playerCount}      Enemy Score:${computerCount}`);  
-        }
-        if(playerCount>computerCount){
-                console.log("You WIN!!");
-        }
-        else if(playerCount===computerCount){
-                console.log("TIE!!");
-        }
-        else{
-                console.log("You LOSE!!");
-        }
+function score(){
+       const yourScore=document.querySelector(".yourscore");
+       const enemyScore=document.querySelector(".enemyscore");
+       yourScore.innerText=`Your:${playerCount}`;
+       enemyScore.innerText=`Enemy:${computerCount}`;
+       if(playerCount===5){
+        const end=document.querySelector(".end");
+        end.innerText=`Your WIN!
+                       Reload page to play again`;
+        end.style.display="flex";
+       }
+       else if(computerCount===5){
+        const end=document.querySelector(".end");
+        end.innerText=`Your WIN!
+                       Reload page to play again`;
+        end.style.display="flex";
+       }
+       roundCount++;
+       return;
 }
 
 let playerCount=0;
 let computerCount=0;
-game();
+let roundCount=0;
+score();
+const choices=document.querySelectorAll("button");
+console.log(choices);
+choices.forEach(choice=>choice.addEventListener("click", playRound));
+
